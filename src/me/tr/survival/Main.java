649d44ae84de850aeb5dc1c268a764ea4ed8a637
TeamRaiderz <nikninza@icloud.com>
2017-05-07 13:28:27 +0300

    Ez

---
+++ src/me/tr/survival/Main.java
@@ -0,0 +1,19 @@
+package me.tr.survival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	public void onEnable(){
+		
+		System.out.println("Plugin on nyt p‰‰ll‰!");
+		
+	}
+	
+	public void onDisable(){
+		
+		System.out.println("Plugin on nyt pois p‰‰lt‰!");
+		
+	}
+	
+}
